d6e10a9e30f7ceefa3333f3887341eacb480080f
Chester <chestermaligaso29@gmail.com>
2024-12-17 12:40:23 +0800

    initialized shadcn setup

--- .history/src/App_20241217122721.jsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="text-lg font-bold">HELLO WORLD!</div>;
-}
-
-export default App;
--- .history/src/App_20241217122842.jsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="text-lg font-bold text-black">HELLO WORLD!</div>;
-}
-
-export default App;
