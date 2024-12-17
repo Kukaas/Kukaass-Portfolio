928c6cdca8765bbe2319ebaba0dfe6ef2f8d1e2e
Chester <chestermaligaso29@gmail.com>
2024-12-17 12:30:07 +0800

    initial commit, set up tailwind css

---
+++ .history/src/App_20241217122721.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="text-lg font-bold">HELLO WORLD!</div>;
+}
+
+export default App;
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="text-lg font-bold text-black">HELLO WORLD!</div>;
+}
+
+export default App;
